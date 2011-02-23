6a1f028396424f3c662ce9e6fe374e3562e93f04
David Santiago <david.santiago@gmail.com>
2011-02-23 01:31:16 -0600

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hew "0.1.0-SNAPSHOT"
+(defproject hew "0.2.0-SNAPSHOT"
   :description "Soy/StringTemplate style templates with Hiccup."
   :dependencies [[clojure "1.2.0"]
                  [hiccup "0.3.4"]])
