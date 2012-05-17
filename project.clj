9fb62590ee9dd883cf9a939f987b61482807128d
David Santiago <david.santiago@gmail.com>
2012-05-17 00:53:12 -0700

    Update project to use hickory 0.1.0.

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Selector-based templates with Hiccup."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hiccup "1.0.0"]
-                 [hickory "0.1.0-SNAPSHOT"]])
+                 [hickory "0.1.0"]])
